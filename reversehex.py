4d54d47a872d89e0ab1f62629da3053ff60cdcc5
Markus Friedl <mfriedl@whiteclarkegroup.com>
2019-12-17 09:07:25 +0100

    calc back to float

--- reversehex.py
+++ reversehex.py
@@ -4,12 +4,23 @@ import struct
 
 def floatToReversedChar(floatvalue):
     radiansint=int(math.radians(floatvalue)*10000000)
+    print(radiansint)
     char1 = (radiansint>>24) & 0xff
     char2 = (radiansint>>16) & 0xff
     char3 = (radiansint>>8) & 0xff
     char4 = radiansint & 0xff
     array=[char4,char3,char2,char1]
     return array
+
+def hexArrayTofloat(hexArray):
+    floatValue=0
+    floatValue=int(hexArray[3])<<24
+    floatValue=floatValue+(int(hexArray[2])<<16)
+    floatValue=floatValue+(int(hexArray[1])<<8)	
+    floatValue=floatValue+int(hexArray[0])
+    floatValue=math.degrees(floatValue/10000000)
+    return floatValue
+
 	
 #-----------------------------------try to encode longitude
 longitude=113.952271
@@ -18,7 +29,9 @@ print('Longitude: '+str(longitude))
 
 #print(hex(char4)+hex(char3)+hex(char2)+hex(char1))
 array=floatToReversedChar(longitude)
+floatagain=hexArrayTofloat(array)
 print(hex(array[0])+hex(array[1])+hex(array[2])+hex(array[3]))
+print(floatagain)
 
 #0x12f7927   #\x30\x79\x2f\x01
 
